46a352160edd01364edf64e813f1f75e89e3d9a4
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2024-03-26 23:38:14 -0500

    Aesthetic tweaks to GPL headers

--- AnsiColor.ps1
+++ AnsiColor.ps1
@@ -1,22 +1,19 @@
 #!/usr/bin/pwsh
 
 <#
-
 This file is part of helpimnotdrowning's PwshUtils.
 
-helpimnotdrowning's PwshUtils is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or (at your option)
-any later version.
+PwshUtils is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
 
-helpimnotdrowning's PwshUtils is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
-License for more details.
+PwshUtils is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
-helpimnotdrowning's PwshUtils. If not, see <https://www.gnu.org/licenses/>. 
-
+PwshUtils. If not, see <https://www.gnu.org/licenses/>.
 #>
 
 class AnsiEscapeCodes {
